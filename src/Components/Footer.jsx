b99f9e03e6e0425d7babc0d9c60ea04c55a94280
Franco46-ochoa <46323573@fi.unju.edu.ar>
2025-05-26 18:50:08 -0300

    Commit Agregado de Footer a lapagina

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import  '../Styles/Styles.css';
+function Footer() {
+  return (
+    <div className="pie_pagina">
+      <div className="footer_izquierda">Facultad de Ingeniería - 2025</div>
+      <div className="footer_derecha">
+        <span className="footer_abreviatura">FIPA</span>
+        <span className="footer_linea_vertical"></span>
+        <span className="footer_nombre_app">Formulario de Informacion Personal de Alumno</span>
+      </div>
+  </div>
+  )
+}
+
+export default Footer
