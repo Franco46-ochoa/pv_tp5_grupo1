89f2aa9637d5e6b1cb5c64ee04bab7beacd0fb89
Franco46-ochoa <46323573@fi.unju.edu.ar>
2025-05-26 19:08:47 -0300

    Commit Agregado de  NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './Styles/Styles.css'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <NavBar/>
       <Footer/>
     </div>
   );
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import  '../Styles/Styles.css'
+
+
+const NavBar = () => {
+  return (
+    <nav className='NavBar'>       
+        <div className='Botones'>
+            <Link to="/">Inicio</Link>
+            <Link to="/Nuevo">Nuevo</Link>
+            <Link to="/Lista">Lista</Link>
+            <Link to="/Acerca">Acerca</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
