73acc9331b0d543e25382fc5ccef03fc993ebdd7
Franco46-ochoa <46323573@fi.unju.edu.ar>
2025-05-26 18:41:15 -0300

    Commit agregado de header a la pagina con su estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import './Styles/Styles.css'
+import Header from './Components/Header';
 
 function App() {
- 
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App
